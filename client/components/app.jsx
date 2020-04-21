cb38bf3d8a4e8ea6c845c3f7369460930536ccaa
Kevin Ihm <kevinihm@gmail.com>
2020-04-21 16:47:35 -0700

    initial setup

---
+++ client/components/app.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    }
+  }
+
+  componentDidMount() {
+
+  }
+
+  getIndicators() {
+    fetch()
+  }
+
+  render() {
+    return null
+  }
+}
