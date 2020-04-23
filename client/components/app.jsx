ec38e2d59fb7cd2f6c0c986ac39311bcef82000d
Kevin Ihm <kevinihm@gmail.com>
2020-04-22 20:58:49 -0700

    added imported components to render method in app

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import Header from './layout/header';
+import Footer from './layout/footer';
 
 export default class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      data: []
     }
   }
 
