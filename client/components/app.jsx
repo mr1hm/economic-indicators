4044d287d2138e754ae90d474a99f3b6e8ae9534
Kevin Ihm <kevinihm@gmail.com>
2020-04-24 18:12:14 -0700

    added logic to handle interestRate data

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -22,19 +22,20 @@ export default class App extends Component {
   getIndicators() {
     const fetchPopulationAndGDP = fetch(`http://api.worldbank.org/v2/country/all/indicator/NY.GDP.MKTP.CD;SP.POP.TOTL?source=2&format=json&per_page=31680`) // Retrieves GDP and Population by Country.
     const fetchLendingInterestRate = fetch(`http://api.worldbank.org/v2/country/all/indicator/FR.INR.LEND?format=json&per_page=15850`)
-    const fetchInflationRate = fetch(`http://api.worldbank.org/v2/country/all/indicator/SL.UEM.TOTL.ZS?format=json&per_page=15850`)
-    const fetchUnemploymentRate = fetch(`http://api.worldbank.org/v2/country/all/indicator/FP.CPI.TOTL.ZG?format=json&per_page=15850`)
+    const fetchUnemploymentRate = fetch(`http://api.worldbank.org/v2/country/all/indicator/SL.UEM.TOTL.ZS?format=json&per_page=15850`)
+    const fetchInflationRate = fetch(`http://api.worldbank.org/v2/country/all/indicator/FP.CPI.TOTL.ZG?format=json&per_page=15850`)
     Promise.all([fetchPopulationAndGDP, fetchLendingInterestRate, fetchInflationRate, fetchUnemploymentRate])
       .then(res => Promise.all(res.map(response => response.json())))
       .then(results => {
         console.log(results)
         // Combine years to 1 object.
-        const dataArray = results[0][1]
-        const combineYears = dataArray.reduce((acc, val, i) => {
+        const GDPandPopulationData = results[0][1]
+        const interestRateData = results[1][1]
+        let reduceGDPandPopulationData = GDPandPopulationData.reduce((acc, val, i) => {
           let yearsObj = {}; // Create empty object literal to store population data in by year.
           if (i > 15839) { // We don't want values from previous indexes, since those contain GDP values.
             // Find first matching country ID.
-            const country = dataArray.find(element => element.country.id === val.country.id)
+            const country = GDPandPopulationData.find(element => element.country.id === val.country.id)
             // Loop through remaining matching countries
             country.populationByYear = { ...country.populationByYear, [val.date]: val.value }
             // Set the year as property in yearsObj
