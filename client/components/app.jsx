c8e0216fce4ce4c06f8398493beca22a905645e9
Kevin Ihm <kevinihm@gmail.com>
2020-04-25 14:31:27 -0700

    added fetch for unemployment rate and inflation rate

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -24,13 +24,15 @@ export default class App extends Component {
     const fetchLendingInterestRate = fetch(`http://api.worldbank.org/v2/country/all/indicator/FR.INR.LEND?format=json&per_page=15850`)
     const fetchUnemploymentRate = fetch(`http://api.worldbank.org/v2/country/all/indicator/SL.UEM.TOTL.ZS?format=json&per_page=15850`)
     const fetchInflationRate = fetch(`http://api.worldbank.org/v2/country/all/indicator/FP.CPI.TOTL.ZG?format=json&per_page=15850`)
-    Promise.all([fetchPopulationAndGDP, fetchLendingInterestRate, fetchInflationRate, fetchUnemploymentRate])
+    Promise.all([fetchPopulationAndGDP, fetchLendingInterestRate, fetchUnemploymentRate, fetchInflationRate])
       .then(res => Promise.all(res.map(response => response.json())))
       .then(results => {
         console.log(results)
         // Combine years to 1 object.
         const GDPandPopulationData = results[0][1]
         const interestRateData = results[1][1]
+        const unemploymentRateData = results[2][1]
+        const inflationRateData = results[3][1]
         let reduceGDPandPopulationData = GDPandPopulationData.reduce((acc, val, i) => {
           let yearsObj = {}; // Create empty object literal to store population data in by year.
           if (i > 15839) { // We don't want values from previous indexes, since those contain GDP values.
