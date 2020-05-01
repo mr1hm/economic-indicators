5b8e8642fb0e90cd163a843783ddbf2771abdb8f
Kevin Ihm <kevinihm@gmail.com>
2020-05-01 12:19:21 -0700

    imported Loading into app

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Header from './layout/header';
 import Footer from './layout/footer';
+import Loading from './layout/loading';
 
 export default class App extends Component {
   constructor(props) {
