05820ee15c6f58329d33c228a14456051e72491f
Kevin Ihm <kevinihm@gmail.com>
2020-05-06 14:39:28 -0700

    added userDashboard.jsx file

---
+++ client/components/userDashboard.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+export default class UserDashboard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    }
+  }
+
+  render() {
+    return (
+      <main className="user-container container-fluid">
+        <section className="row">
+          <div className="col">
+
+          </div>
+        </section>
+      </main>
+    );
+  }
+}
