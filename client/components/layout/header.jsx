b8619d046645ec63c2f6c75a88c4704ec3b5e476
Kevin Ihm <kevinihm@gmail.com>
2020-05-05 19:46:32 -0700

    changed main element to header element in Header component

--- client/components/layout/header.jsx
+++ client/components/layout/header.jsx
@@ -10,13 +10,13 @@ export default class Header extends Component {
 
   render() {
     return (
-      <main className="header-container container-fluid">
+      <header className="header-container container-fluid">
         <section className="row">
           <div className="col d-flex">
             <h1 className="header-title">Economic Indicators</h1>
           </div>
         </section>
-      </main>
+      </header>
     );
   }
 }
