47884a68b76a889def919e3d796162b86f5a5ba8
Kevin Ihm <kevinihm@gmail.com>
2020-05-06 16:05:14 -0700

    deconstructed entirety of state in app

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -149,8 +149,13 @@ export default class App extends Component {
       .catch(err => console.error(err));
   };
 
+  handleCountrySelect(e) {
+    const name = e.target.name, value = e.target.value
+    this.setState({ [name]: value })
+  }
+
   render() {
-    const { GDPandPopulation, UnemploymentRate, InflationRate } = this.state.data;
+    const { data: { GDPandPopulation, UnemploymentRate, InflationRate }, countryView } = this.state;
     if (this.state.loading) {
       return (
         <Loading loading={this.state.loading} />
