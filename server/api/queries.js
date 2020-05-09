216ec40f8871a85c37804d27f003949a2bac0282
Kevin Ihm <kevinihm@gmail.com>
2020-05-09 16:26:08 -0700

    added post database query to store country names and iso3 codes

--- server/api/queries.js
+++ server/api/queries.js
@@ -1,2 +1,24 @@
 const Pool = require('pg').Pool;
 const conn = require('./db_conn');
+
+const storeCountries = (req, res, next) => {
+  const { countries } = req.body
+  console.log(countries)
+  for (let i = 0; i < countries.length; i++) {
+    conn.query(`INSERT INTO countries (name, country_iso3_code) VALUES ($1, $2) returning *`, [countries[i].countryName, countries[i].countryiso3code], (err, results) => {
+      if (err) throw err;
+      res.status(201)
+    })
+  }
+}
+
+const getAllData = (req, res, next) => {
+  conn.query(`select * from data`)
+    .then(result => res.json(result.rows[0]))
+    .catch(err => next(err))
+}
+
+
+module.exports = {
+  storeCountries,
+}
